5e56ae450f1c509ca6ba99b7f72568c792a01ca7
HaierKeys <haierkeys@gmail.com>
2026-02-12 05:38:55 +0800

    去除小语种语言支持

--- src/lib/i18n/translations.ts
+++ src/lib/i18n/translations.ts
@@ -9,29 +9,18 @@ import vi from "./locales/vi";
 import uk from "./locales/uk";
 import tr from "./locales/tr";
 import th from "./locales/th";
-import sq from "./locales/sq";
 import ru from "./locales/ru";
-import ro from "./locales/ro";
 import pt from "./locales/pt";
 import pl from "./locales/pl";
-import no from "./locales/no";
 import nl from "./locales/nl";
-import ne from "./locales/ne";
-import ms from "./locales/ms";
 import ko from "./locales/ko";
 import ja from "./locales/ja";
 import it from "./locales/it";
 import id from "./locales/id";
-import hu from "./locales/hu";
-import hi from "./locales/hi";
-import he from "./locales/he";
 import fr from "./locales/fr";
 import es from "./locales/es";
 import en from "./locales/en";
 import de from "./locales/de";
-import da from "./locales/da";
-import ca from "./locales/ca";
-import be from "./locales/be";
 import ar from "./locales/ar";
 
 
@@ -42,11 +31,8 @@ i18n.use(initReactI18next).init({
     ja: { translation: ja },
     ko: { translation: ko },
     "zh-TW": { translation: zhTW },
-    hi: { translation: hi },
     id: { translation: id },
-    ms: { translation: ms },
     th: { translation: th },
-    ne: { translation: ne },
     vi: { translation: vi },
     tr: { translation: tr },
     fr: { translation: fr },
@@ -57,17 +43,9 @@ i18n.use(initReactI18next).init({
     "pt-BR": { translation: ptBR },
     nl: { translation: nl },
     pl: { translation: pl },
-    da: { translation: da },
-    no: { translation: no },
-    ro: { translation: ro },
-    ca: { translation: ca },
-    hu: { translation: hu },
     uk: { translation: uk },
-    be: { translation: be },
     ru: { translation: ru },
     ar: { translation: ar },
-    he: { translation: he },
-    sq: { translation: sq },
   },
   lng: getBrowserLang(),
   fallbackLng: "zh-CN",
